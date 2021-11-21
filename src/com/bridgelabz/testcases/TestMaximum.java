d0d64fb5e28884e71b3ff1abf7dc6967d70d64c6
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-21 14:44:09 +0530

    finding maximum of 3 strings

--- src/com/bridgelabz/testcases/TestMaximum.java
+++ src/com/bridgelabz/testcases/TestMaximum.java
@@ -30,30 +30,56 @@ public class TestMaximum {
 		if(maxCheck>0) {
 			maxCheck=intArr[0].compareTo(intArr[2]);
 			if(maxCheck>0) {
-				System.out.println("maximum of three integers is : " + intArr[0]);
+				System.out.println("maximum of three doubles is : " + intArr[0]);
 			}
 			else
-				System.out.println("maximum of three integers is : " + intArr[2]);
+				System.out.println("maximum of three doubles is : " + intArr[2]);
 			
 		}else if(maxCheck<0) {
 			maxCheck=intArr[1].compareTo(intArr[2]); 
 			if(maxCheck>0) {
-				System.out.println("maximum of three integers is : " + intArr[1]);
+				System.out.println("maximum of three doubles is : " + intArr[1]);
 			}
 			else
-				System.out.println("maximum of three integers is : " + intArr[2]);
+				System.out.println("maximum of three doubles is : " + intArr[2]);
 		}
+	}
+	
+	private static void maximumSting(String[] stringArray) {
+		int maxCheck = stringArray[0].compareTo(stringArray[1]);
+		if(maxCheck>0) {
+			maxCheck=stringArray[0].compareTo(stringArray[2]);
+			if(maxCheck>0) {
+				System.out.println("maximum of three strings is : " + stringArray[0]);
+			}
+			else
+				System.out.println("maximum of three strings is : " + stringArray[2]);
+			
+		}else if(maxCheck<0) {
+			maxCheck=stringArray[1].compareTo(stringArray[2]); 
+			if(maxCheck>0) {
+				System.out.println("maximum of three strings is : " + stringArray[1]);
+			}
+			else
+				System.out.println("maximum of three strings is : " + stringArray[2]);
+		}	
 	}
 	
 	public static void main(String[] args) {
 		
 		Integer intArray[] = {20,22,21};
-		Double doubleArray[] = {29.26,22.25,28.23};//TC-1.1 :- max num at first position.
+		Double doubleArray[] = {26.26,22.25,28.23};
+		
 		maximumInteger(intArray);
 		maximumDouble(doubleArray);
-		Double doubleArray1[]= {12.1, 25.8, 17.7}; //TC-1.2 :- max num at second position.
-		maximumDouble(doubleArray1);
-		Double doubleArray2[]= {15.3, 15.8, 20.56}; //TC-1.3 :- max num at third position.
-		maximumDouble(doubleArray2);
+		
+		/** Test cases for Strings. **/
+		String stringArray[]={"Peach", "Apple", "Banana"};  //TC-1.1 :- max string at first position.
+		String stringArray1[]={"car", "cycle", "bike"};  //TC-1.2 :- max string at second position.
+		String stringArray2[]={"bat", "ball", "wickets"}; //TC-1.3 :- max string at third position. 
+		maximumSting(stringArray);
+		maximumSting(stringArray1);
+		maximumSting(stringArray2);
+	
 	}
 }
